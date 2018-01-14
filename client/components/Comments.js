b71ce14cefd1b161511110618550b7efbc759692
Clare <chblackburne@gmail.com>
2018-01-14 12:26:33 +0000

    Displays single photo component using index and params from url

---
+++ client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render(){
+
+  return(
+    <div>I'm a comment</div>
+  )
+  }
+
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render(){
+    //index of post
+    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    //gets us post
+    const post = this.props.posts[index];
+
     return(
      <div className="single-photo">
-       I'm the single photo
+       <Photo index={index} post={post} {...this.props}/>
+       <Comments></Comments>
      </div>
     )
   }
-
 });
 
 export default Single;
