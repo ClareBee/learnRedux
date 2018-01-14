2796439ee5950bf512cb59f7471a34e14edbdedb
Clare <chblackburne@gmail.com>
2018-01-14 13:39:31 +0000

    Debugs flexbox on photo grid component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -5,7 +5,7 @@ import Photo from './Photo';
 const PhotoGrid = React.createClass({
   render(){
     return(
-     <div className="photogrid">
+     <div className="photo-grid">
        {this.props.posts.map((post, index) => <Photo {...this.props} key={index} index={index} post={post} />)}
      </div>
     )
