95cdce21421a00ab45ee5fe1f3e2a86cfda39abc
Clare <chblackburne@gmail.com>
2018-01-14 10:26:50 +0000

    Adds photo component and loops over photos in the grid

---
+++ client/components/Photo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Photo = React.createClass({
+  render(){
+    return(
+      <figure className="grid-figure">
+        I am a photo
+      </figure>
+    )
+  }
+});
+export default Photo;
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render(){
     return(
      <div className="photogrid">
-       I'm the photogrid photo
+       {this.props.posts.map((post, index) => <Photo />)}
      </div>
     )
   }
