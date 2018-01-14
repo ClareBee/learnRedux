c00c255c00e1a10326368541b4e48ce0e8bc8340
Clare <chblackburne@gmail.com>
2018-01-14 11:00:27 +0000

    Connects button click to increment state, using bind to allow index to be passed in

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -5,7 +5,7 @@ import CSSTransitionGroup from 'react-addons-css-transition-group';
 const Photo = React.createClass({
   render(){
     //creates 3 separate variables
-    const { post, i, comments } = this.props;
+    const { post, index, comments } = this.props;
     return(
       <figure className="grid-figure">
         <div className="grid-photo-wrap">
@@ -20,11 +20,11 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button className="likes">&hearts; {post.likes}</button>
+            <button onClick={this.props.increment.bind(null, index)} className="likes">&hearts; {post.likes}</button>
             <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>
-                {comments[post.code] ? comments[post.code] : 0}
+                {comments[post.code] ? comments[post.code].length : 0}
               </span>
             </Link>
           </div>
