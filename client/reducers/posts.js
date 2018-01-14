fc47e51e7ade2896a48f77cd500511490ee9343a
Clare <chblackburne@gmail.com>
2018-01-14 11:10:17 +0000

    Updates state in post reducer to alter only the post that has been changed

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -5,8 +5,19 @@
 
 //ES6 default parameters
 function posts(state = [], action){
-  console.log("the post will change");
-  console.log(state, action);
+  //type = increment_likes
+  switch(action.type){
+    case 'INCREMENT_LIKES':
+    const i = action.i;
+    //return updated state as a new array using ES6 spread to create a copy of the altered item
+    return [
+      ...state.slice(0, i), //before the one we're updating
+      { ...state[i], likes: state[i].likes + 1 },
+      ...state.slice(i + 1) //everything after it
+    ]
+    default:
+    return state;
+  }
   return state;
 }
 
